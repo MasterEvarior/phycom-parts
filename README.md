eeb8f09e7b90ef604c468c70728b6cab2067762f
Giannin <contact@giannin.ch>
2026-06-10 21:38:11 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PhyCom Parts
+TODO
+
+## Build
+Because there are no external dependencies and nothing to build, not build system is needed.
+
+Simply move the `src` folder to your webserver of choice and see the magic happen.
+
+## External Resources
+- [GitHub Actions for CICD](https://github.com/features/actions)
+- [JetBrains Mono Font](https://fonts.google.com/specimen/JetBrains+Mono)
---
+++ src/index.js
@@ -0,0 +1,87 @@
+/**
+ * @typedef {Object} Part
+ * @property {string} name - The name of the component.
+ * @property {string} source - The wiki or source URL where the component is listed.
+ * @property {'actuator' | 'sensor' | 'adapter' | 'cable'} category - The category classification of the component.
+ * @property {string} image - The URL to an image of the component.
+ */
+
+export const loadData = async () => {
+  const response = await fetch("../data.json");
+  return await response.json();
+};
+
+/**
+ * Sets the output area content.
+ * @param {string} html - The HTML content to display.
+ */
+export const setOutput = (html) => {
+  console.log(html);
+  document.getElementById("output").innerHTML = html;
+};
+
+/**
+ * Executes a command based on user input.
+ * Searches for a matching command by name, otherwise falls back to the default command.
+ * @param {string} input - The command input by the user.
+ * @param {Array<Command>} commands - The list of available commands.
+ * @returns {string} - The output of the command
+ */
+export const executeCommand = (input, commands) => {
+  const command =
+    commands.find((c) => c.names.includes(input)) ??
+    getDefaultCommand(commands);
+  return command.output(input);
+};
+
+/**
+ * Create a single row, out of a single part
+ * @param {Part} part - Single part to display.
+ * @returns {string} - A single <tr>.
+ */
+export const constructRow = (part) => {
+  return `
+    <tr>
+      <td>
+        <img src="${part.image}" alt=${part.name}>
+      </td>
+      <td>${part.name}</td>
+      <td>${part.category}</td>
+      <td>
+        <a href="${part.source}">Wiki</td>
+    </tr>
+  `;
+};
+
+/**
+ * Construct result table with all parts
+ * @param {Part[]} parts - All parts to display.
+ * @returns {string} - A complete <table>.
+ */
+export const constructTable = (parts) => {
+  return `
+    <table>
+      <tr>
+        <th>Image</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Source</th>
+      </tr>
+      ${parts.map((p) => constructRow(p)).reduce((a, b) => a + " " + b, "")}
+    </table>
+  `;
+};
+
+/**
+ * Initializes the command input listener and sets up event handling.
+ * Listens for the Enter key press to execute user input commands.
+ */
+export const setup = async () => {
+  const data = await loadData();
+
+  const prompt = document.getElementById("input");
+  prompt.addEventListener("keyup", (e) => {
+    const result = constructTable(data);
+    setOutput(result);
+  });
+};
